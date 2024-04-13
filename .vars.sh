43dd62227ef2e74625183a4ebe98b84d816cfe2e
agus1985 <agus1985@hotmail.com>
2024-04-13 13:17:17 -0600

    Create .vars.sh

---
+++ .vars.sh
@@ -0,0 +1,6 @@
+export apiToken=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
+export apiUrl="https://api.github.com"
+export emailRecipientsReport="target1@example.com,target2@example.com"
+export githubUser="zalando"
+export githubRepo="patroni"
+export emailSender="report_sender@example.com"
